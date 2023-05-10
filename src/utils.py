6678d86342e171188cd71d60d74ac387fb2eae39
yvr354 <venkatrakesh354@gmail.com>
2023-05-10 09:24:37 +0200

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ from src.exception import CustomException
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -19,14 +20,22 @@ def save_object(file_path, obj):
         raise CustomException(e,sys)      
 
 
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+def evaluate_models(x_train,y_train,x_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            model.fit(x_train, y_train) #Train model
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+            #print(gs.best_estimator_)
+            #print(gs.best_params_)
+
+            #model.fit(x_train, y_train) #Train model when we use grid search we dont need this 
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
 
             y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
